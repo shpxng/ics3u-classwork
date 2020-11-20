b625ce803c30cb2c9ad05c3df8f333d71f8742f6
shpxng <74629751+shpxng@users.noreply.github.com>
2020-11-20 13:06:43 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,5 +1,5 @@
 # Ask user for favourite color
-colour = input("What is your favourite colour?")
+colour = input("What is your favourite colour? ")
 
 # Output a message
 print("Wow,", colour, "I also love that colour!")
