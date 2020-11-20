9c4d8449b9de86863a2a7ef79882e3acf53b2920
shpxng <74629751+shpxng@users.noreply.github.com>
2020-11-20 13:52:17 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -4,7 +4,7 @@ colour = input("What is your favourite colour? ")
 print(f"{colour}? That's my favourite colour too!")
 
 # Question 2
-can = int(input("How many cans come in a pack? "))
+can_in_pack = int(input("How many cans come in a pack? "))
 
 pack = int(input("How many packs are there? "))
 
