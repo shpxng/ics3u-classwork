2af764f10a651363820959328c74dc5bc0151e77
shpxng <74629751+shpxng@users.noreply.github.com>
2020-11-20 13:22:48 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,5 +1,25 @@
+1.
 # Ask user for favourite color
 colour = input("What is your favourite colour? ")
 
 # Output a message
-print("Wow,", colour, "I also love that colour!")
+print(colour, "That's my favourite colour too!")
+
+2.
+# Ask user for number of cans
+can = int(input("How many cans come in a pack? "))
+
+# Ask user for number of packs
+pack = int(input("How many packs are there? "))
+
+# Display total number of cans
+print("The total number of cans is", can * pack)
+
+3.
+# Ask user for dimensions of a rectangular prism
+length = int(input("What is the length of the prism in cm? "))
+width = int(input("What is the width of the prism in cm? "))
+height = int(input("What is the height of the prism in cm? "))
+
+# Display the volume of the prism
+print("The volume of the prism is", length * width * height, "cm squared")
