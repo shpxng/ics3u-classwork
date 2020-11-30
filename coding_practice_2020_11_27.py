31d5c1b53dd48f36679e52c8490ad06a5905c1cc
shpxng <74629751+shpxng@users.noreply.github.com>
2020-11-29 21:37:05 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,59 @@
+# 1. from 142 to 380 by 7.
+i = 142
+while i <= 380:
+    print(i)
+    i += 7
+
+# 2. from -327 to 50 by 13.
+i = -327
+while i<= 50:
+    print(i)
+    i += 13
+
+# 3. from 291 to 606 by 15.
+i = 291
+while i<= 606:
+    print(i)
+    i += 15
+
+# 4. from 455 to 627 by 4.
+i = 455
+while i <= 627:
+    print(i)
+    i += 4
+
+# 5. from -108 to 340 by 14.
+i = -108
+while i <= 340:
+    print(i)
+    i += 14
+
+# 6. from -494 to -18 by 17.
+i = -494
+while i <= -18:
+    print(i)
+    i += 17
+
+# 7. from -121 to 199 by 8.
+i = -121
+while i <= 199:
+    print(i)
+    i += 8
+
+# 8. from 271 to 1088 by 19.
+i = 271
+while i <= 1088:
+    print(i)
+    i += 19
+
+# 9. from 447 to 993 by 13.
+i = 447
+while i <= 993:
+    print(i)
+    i += 13
+
+# 10. from 93 to 877 by 16.
+i = 93
+while i <= 877:
+    print(i)
+    i += 16
